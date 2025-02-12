523ba122a59dedfbee322b8a6f2591e397cb5335
jonathan <jonathhu@stud.ntnu.no>
2025-02-12 14:20:45 +0100

    Add crossorigin

--- src/main/java/stud/ntnu/no/calculator/controller/RestController.java
+++ src/main/java/stud/ntnu/no/calculator/controller/RestController.java
@@ -3,6 +3,7 @@ package stud.ntnu.no.calculator.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class RestController {
   public RestController(CalculatorService service) {
     this.service = service;
   }
-
+  @CrossOrigin(origins = "http://localhost:5173/")
   @PostMapping("/calculate")
   public CalculationResponse calculate(@RequestBody CalculationRequest request) {
     logger.info("Received calculation request: {} {} {}", request.getNum1(), request.getOperator(), request.getNum2());
